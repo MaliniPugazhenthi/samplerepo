fd2e64ef235d749769c499538fff36717d52ad0f
MaliniPugazhenthi <malinipugazhenthi2911@gmail.com>
2024-07-22 07:45:02 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,27 @@
+prin= "ghffdd"
+pt = 76544
+
+#dict
+dc = {"ap":1,"lk":7,"pu":"odhhfh"}
+
+ls= [3,'pp',{"ap":1,"lk":7,"pu":[7,9,{"yu":22}]}]
+print(ls[2]['pu'][2]["yu"])
+
+hj= (6,8,"kl",677)
+
+for i in hj:
+    print(i)
+
+
+# print(dc['pu'])
+# print(dc.keys())
+# print(dc.values())
+
+# for i in dc.keys():
+#     print(i)
+
+# for i in dc.values():
+#     print(i)
+
+
+
